b167cbbcc88a40643a657b0370e6d631ab14588c
Ebin Krishnan <ebinkrishnan012@gmail.com>
2026-09-13 18:41:57 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-s3-bucket-for-terraform-state-ebink"
+    key            = "terraform/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "my-dynamo-db-table-tf"
+  }
+}
