df377a11d114094ccd85a2c83cb2c4f2688c6943
Ebin Krishnan <ebinkrishnan012@gmail.com>
2026-09-13 19:14:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "my-s3-bucket-for-terraform-state-ebink"
     key            = "terraform/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamo-db-table-terrafomr-ebi"
   }
 }
